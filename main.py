b5f56fb0f250a964aba8ba07710e66d0775464e3
Emzycodes <123588480+Emzycodes@users.noreply.github.com>
2023-11-19 02:10:25 +0100

    Updating the RandomWalk repo

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
 import matplotlib.pyplot as plt
-from raandomwalks import RandomWalks
+from randomwalks import RandomWalks  # Fix the typo in the import
 
-# Make a random walk
-rw = RandomWalks()
-rw.fill_walk()
+def run_visual():
+    repeat = True
+    
+    while repeat:
+        # Make a random walk
+        rw = RandomWalks()
+        rw.fill_walk()
 
-# Plot the points in the walk.
-plt.style.use('classic')
-fig , ax = plt.subplots()
-ax.scatter(rw.x_values,rw.y_values,s=15)
-plt.savefig("img.png" ,bbox_inches='tight')
-plt.show()
+        # Plot the points in the walk.
+        plt.style.use('classic')
+        fig, ax = plt.subplots()
+        ax.scatter(rw.x_values, rw.y_values, s=15)
+        plt.savefig("img.png", bbox_inches='tight')
+        plt.show()
+
+        restart = input("Do you want to run the analysis again? (y/n): ").lower()
+
+        if restart == "n":
+            print("Exiting visualization.")
+            repeat = False
+        elif restart == "y":
+            print("Restarting visualization...")
+        else:
+            print("Invalid input. Exiting visualization.")
+            repeat = False
+
+# Call the run_visual() function to execute the visualization
+if __name__ == "__main__":
+    run_visual()
